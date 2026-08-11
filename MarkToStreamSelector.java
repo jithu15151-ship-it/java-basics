ad4c22efdcbbd5b6c15d4d613288050c2176a040
jithu15151-ship-it <jithu15151@gmail.com>
2026-08-11 19:10:56 +0530

    Rename program13.java to MarkToStreamSelector.java - Map marks to course streams

---
+++ MarkToStreamSelector.java
@@ -0,0 +1,18 @@
+package com.mycompany.mark;
+
+import java.util.Scanner;
+
+public class MarkToStreamSelector {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER A MARK:");
+        int mark =sc.nextInt(); 
+    String marks = (mark>85)?"medical course":
+            (mark>=75)?"Engineering course":
+            (mark>=60)?"arts and science":
+            (mark>=40)?"diplamo":
+            (mark>=35)?"pass":"fail";
+    System.out.println(marks);
+    }
+}
